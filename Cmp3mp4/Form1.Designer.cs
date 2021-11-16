883a6d9155ccb06ea5f05cb0e62c94b09f21df0c
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2021-11-16 23:09:16 +0300

    Add project files.

---
+++ Cmp3mp4/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Cmp3mp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtSaveTo = new System.Windows.Forms.TextBox();
+            this.txtOpenFile = new System.Windows.Forms.TextBox();
+            this.btnSaveTo = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.txtSaveTo);
+            this.panel1.Controls.Add(this.txtOpenFile);
+            this.panel1.Controls.Add(this.btnSaveTo);
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(763, 204);
+            this.panel1.TabIndex = 0;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(230, 131);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "MP4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(161, 131);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MP3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSalmon;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(729, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(22, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LightGreen;
+            this.btnStart.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(330, 155);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 34);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtSaveTo
+            // 
+            this.txtSaveTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSaveTo.Location = new System.Drawing.Point(156, 85);
+            this.txtSaveTo.Name = "txtSaveTo";
+            this.txtSaveTo.Size = new System.Drawing.Size(546, 26);
+            this.txtSaveTo.TabIndex = 3;
+            // 
+            // txtOpenFile
+            // 
+            this.txtOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOpenFile.Location = new System.Drawing.Point(156, 47);
+            this.txtOpenFile.Name = "txtOpenFile";
+            this.txtOpenFile.Size = new System.Drawing.Size(547, 26);
+            this.txtOpenFile.TabIndex = 2;
+            // 
+            // btnSaveTo
+            // 
+            this.btnSaveTo.Location = new System.Drawing.Point(74, 79);
+            this.btnSaveTo.Name = "btnSaveTo";
+            this.btnSaveTo.Size = new System.Drawing.Size(75, 32);
+            this.btnSaveTo.TabIndex = 1;
+            this.btnSaveTo.Text = "Save To";
+            this.btnSaveTo.UseVisualStyleBackColor = true;
+            this.btnSaveTo.Click += new System.EventHandler(this.btnSaveTo_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(74, 41);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 32);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "About";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(763, 445);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mp3Mp4 By Ali Code";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSaveTo;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtSaveTo;
+        private System.Windows.Forms.TextBox txtOpenFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Cmp3mp4/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cmp3mp4
+{
+    public partial class Form1 : Form
+    {
+        public string formatType;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("test");
+            //open file
+            //openFileDialog1.Filter = "ogg files (*.ogg)|*.ogg|All files (*.*)|*.*";
+            openFileDialog1.InitialDirectory = "C:\\Users\\%USERPROFILE%\\Downloads";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+
+            txtOpenFile.Text = openFileDialog1.FileName;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSaveTo_Click(object sender, EventArgs e)
+        {
+            // save file
+
+           
+
+            if(radioButton1.Checked)
+            {
+                formatType = ".mp3";
+            }
+            else
+            {
+                formatType = ".mp4";
+            }
+
+            saveFileDialog1.FileName = Regex.Replace(openFileDialog1.SafeFileName, @"\.\w+", "")+ formatType;
+
+            saveFileDialog1.ShowDialog();
+
+            txtSaveTo.Text = saveFileDialog1.FileName;
+
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //START
+
+            string strCmdText;
+            if (radioButton1.Checked)
+            {
+                formatType = ".mp3";
+            }
+            else
+            {
+                formatType = ".mp4";
+            }
+
+            if (openFileDialog1.FileNames.Length>1)
+            {
+
+                Console.WriteLine("you select more then one file");
+                int i = 0;
+                while (openFileDialog1.FileNames.Length > i)
+                {
+                    strCmdText = "ffmpeg -i \"" + openFileDialog1.FileNames[i] + "\" \"" + Directory.GetCurrentDirectory() + "\\FFOutput\\ " + Regex.Replace(openFileDialog1.SafeFileNames[i], @"\.\w+", "") + formatType + "\"";
+                    System.Diagnostics.Process.Start("CMD.exe", "/c " + strCmdText);
+                   
+                    Console.WriteLine(strCmdText);
+                    i++;
+                }
+
+                System.Diagnostics.Process.Start("explorer.exe", Directory.GetCurrentDirectory() + "\\FFOutput\\ ");
+
+
+
+            }
+            else
+            {
+                
+
+                strCmdText = "ffmpeg -i \"" + txtOpenFile.Text + "\" \"" + txtSaveTo.Text + "\"";
+                Console.WriteLine(strCmdText);
+                System.Diagnostics.Process.Start("CMD.exe", "/c " + strCmdText);
+
+
+               // System.Diagnostics.Process.Start("explorer.exe", saveFileDialog1.FileName);
+            }
+
+
+            
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Code By Ali Alhashim (ali Code) tools: C#+WindowsForms +ffmpeg ");
+        }
+    }
+}
